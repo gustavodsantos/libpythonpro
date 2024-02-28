13e9113c9cdc5f2b1d530f55f9fa905352bc225e
gustavodsantos <gustavojuniordos@hotmail.com>
2024-02-27 22:45:13 -0300

    Biblioteca Pytest-Mock

--- libpythonpro/tests/test_spam/test_github_api.py
+++ libpythonpro/tests/test_spam/test_github_api.py
@@ -1,15 +1,29 @@
 from unittest.mock import Mock
 
+import pytest
+
 from libpythonpro import github_api
 
 
-def test_buscar_avatar():
+@pytest.fixture
+def avatar_url(mocker):
     resp_mock = Mock()
+    url = 'https://avatars.githubusercontent.com/u/131466188?v=4'
     resp_mock.json.return_value = {
         'login': 'gustavodsantos',
         'id': 131466188,
-        'avatar_url': 'https://avatars.githubusercontent.com/u/131466188?v=4',
+        'avatar_url': url,
     }
-    github_api.requests.get = Mock(return_value=resp_mock)
+    get_mock = mocker.patch('libpythonpro.github_api.requests.get')
+    get_mock.return_value = resp_mock
+    return url
+
+
+def test_buscar_avatar(avatar_url):
     url = github_api.buscar_avatar('gustavodsantos')
-    assert 'https://avatars.githubusercontent.com/u/131466188?v=4' == url
+    assert avatar_url == url
+
+
+def test_buscar_avatar_integracao():
+    url = github_api.buscar_avatar('renzon')
+    assert 'https://avatars.githubusercontent.com/u/3457115?v=4' == url
